3f3b2dc878584ca28b9304853fc8f2b4416a43c2
Dipanshu raj <87578029+Dipansu02654@users.noreply.github.com>
2023-12-24 01:34:33 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,7 @@
+define ('url',"https://api.telegram.org/bot6834718836:AAFSF4bC8q9aUwXMS1hvnKaP4z230JnN8n8");
+$adress = $_GET['adress'];
+$cart = $_GET['cart'];
+$chat_id = '2099878356';
+$message = urlencode("cart:".$cart."\n adress : ".$adress);
+file_get_contents(url."sendmessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
+
